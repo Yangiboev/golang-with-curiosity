e722884e2f906bfab2294af65cac17ce65f7ad1e
Dilmurod Yangiboev <dilmurod.yangiboev@gmail.com>
2021-10-23 21:27:51 +0500

    usecase delivery changed

--- internal/product/delivery.go
+++ internal/product/delivery.go
@@ -1,5 +1,7 @@
 package product
 
+import "github.com/labstack/echo/v4"
+
 // HttpDelivery http delivery
 type HttpDelivery interface {
 	CreateProduct() echo.HandlerFunc
--- internal/product/usecase.go
+++ internal/product/usecase.go
@@ -13,7 +13,7 @@ type UseCase interface {
 	Create(ctx context.Context, product *models.Product) (*models.Product, error)
 	Update(ctx context.Context, product *models.Product) (*models.Product, error)
 	GetByID(ctx context.Context, productID primitive.ObjectID) (*models.Product, error)
-	Search(ctx context.Context, search string, pagination *utils.PaginationUC) (*models.ProductsList, error)
+	Search(ctx context.Context, search string, pagination *utils.Pagination) (*models.ProductsList, error)
 	PublishCreate(ctx context.Context, product *models.Product) error
 	PublishUpdate(ctx context.Context, product *models.Product) error
 }
